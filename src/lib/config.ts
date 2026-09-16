98164035418bb690835ed3545bb5ecc6348261cb
Muhammad sani shehu <ssghaany@gmail.com>
2026-09-16 13:32:58 +0100

    Add configuration constants for Monnify API

---
+++ src/lib/config.ts
@@ -0,0 +1,3 @@
+export const BADGE_FEE_NAIRA = 5000; // change this to whatever you want to charge
+export const MONNIFY_API_KEY = import.meta.env.VITE_MONNIFY_API_KEY as string;
+export const MONNIFY_CONTRACT_CODE = import.meta.env.VITE_MONNIFY_CONTRACT_CODE as string;
